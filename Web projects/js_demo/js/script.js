53334a43c31eb3c63098f3d620a75a1bcb10d330
BlueChairs <nlatuskiewicz@eastsideprep.org>
2023-11-02 10:37:09 -0700

    6767

--- Web projects/js_demo/js/script.js
+++
@@ -1,6 +0,0 @@
-function Czech(){
-    var p = prompt("Now tell me how many ways of saying hello informally were there?");
-    if (p != null) {
-        document.getElementById('question').innerHTML = "Wrong"
-    }
-}
